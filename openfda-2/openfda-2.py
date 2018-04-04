6f975c3f39abc87337539af0f97937c65c5ea45e
joaquingalant <37300256+joaquingalant@users.noreply.github.com>
2018-04-04 13:35:53 +0200

    Update openfda-2.py

--- openfda-2/openfda-2.py
+++ openfda-2/openfda-2.py
@@ -1,1 +1,26 @@
+import http.client
+import json
 
+headers = {"User-Agent": "http-client"}
+
+REST_SERVIDOR= "api.fda.gov"
+REST_RECURSO = "/drug/label.json"
+CONSULTA= "/?search=active_ingredient:acetylsalicylic&limit=20"
+
+connector = http.client.HTTPSConnection(REST_SERVIDOR)
+connector.request("GET", REST_RECURSO + CONSULTA, None, headers)
+
+response1 = connector.getresponse()
+aspirina = response1.read().decode("utf-8")
+connector.close()
+
+aspirins = json.loads(aspirina)["results"]
+
+
+for aspirin in aspirins:
+    print("ID: ", aspirin["id"])
+    if aspirin["openfda"]:
+        manufacturer = aspirin["openfda"]["manufacturer_name"]
+        print("FABRICANTE: ",manufacturer)
+    else:
+        print("Fabricante no disponible")
