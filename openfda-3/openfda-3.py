136c4ada1af07e80bcc6bd33893b61c19a460bfd
joaquingalant <37300256+joaquingalant@users.noreply.github.com>
2018-04-04 17:49:45 +0200

    Update openfda-3.py

--- openfda-3/openfda-3.py
+++ openfda-3/openfda-3.py
@@ -1,1 +1,16 @@
+import http.client
+import json
+import socket
 
+headers = {"User-Agent" : "http-client"}
+SERVIDOR = "api.fda.gov"
+RECURSO = "/drug/label.json"
+LIMIT = "?limit=10"
+connector = http.client.HTTPSConnection(SERVIDOR)
+connector.request("GET", RECURSO + LIMIT, None, header)
+
+datos10 = connector.getresponse()
+medicine = datos10.read().decode("utf-8")
+connector.close()
+
+results = json.loads(medicine)['results']
