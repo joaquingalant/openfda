4495f749041ee8627b166e37cd6593810f1b0d6e
joaquingalant <37300256+joaquingalant@users.noreply.github.com>
2018-04-05 16:56:07 +0200

    Update Practica-2.py

--- openfda-2/Practica-2.py
+++ openfda-2/Practica-2.py
@@ -1,13 +1,12 @@
-import http.client
+import http.client #Importamos los módulos que necesitaremos
 import json
 
-headers = {"User-Agent": "http-client"}
-
+headers = {"User-Agent": "http-client"} #Nombramos parametros de nuevo, en la CONSULTA ponemos limite=100, más allá de ese número saltará un error
 REST_SERVIDOR= "api.fda.gov"
 REST_RECURSO = "/drug/label.json"
 CONSULTA= "/?search=active_ingredient:acetylsalicylic&limit=100"
 
-connector = http.client.HTTPSConnection(REST_SERVIDOR)
+connector = http.client.HTTPSConnection(REST_SERVIDOR) #Establecemos conexion, lo leemos e interpretamos en json
 connector.request("GET", REST_RECURSO + CONSULTA, None, headers)
 
 response1 = connector.getresponse()
@@ -16,11 +15,10 @@ connector.close()
 
 aspirins = json.loads(aspirina)["results"]
 
-
-for aspirin in aspirins:
+for aspirin in aspirins: #Encontramos aquellos con acetilsalicilico como ingrediente activo, e impriimos sus ID y fabricantes
     print("ID: ", aspirin["id"])
     if aspirin["openfda"]:
         manufacturer = aspirin["openfda"]["manufacturer_name"]
         print("FABRICANTE: ",manufacturer)
-    else:
+    else: #En caso de no disponer la información necesaria, se imprimirá un mensaje por pantalla con el siguiente mensaje:
         print("Fabricante no disponible")
